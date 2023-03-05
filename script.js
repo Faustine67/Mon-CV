36f9f7ed30827db8770bcb8c6b1fae98cf87f3eb
Faustine67 <michelfaustine@gmail.com>
2023-03-05 17:32:39 +0100

    Add scroll condition

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+//NAVBAR //
+let lastScrollTOP=0;
+navbar= document.getElementById('navbar');
+
+window.addEventListener('scroll',function() {
+    const scrollTop = window.pageTOffset ||
+    this.document.documentElement.scrollTop;
+
+// si scrollTOp est sup à la derniere fois que l'on a scrollé (soit lorsque l'on remonte, soit lorsque l'on descend) alors la nav bar disparait
+// Pour faire disparaitre la nav bar, on agit sur son style, en la faisant remonter de 50px
+if (scrollTop > lastScrollTop) {
+        nav.style.top="-50px";
+    } else{
+        navbar.style.top="0";
+    }
+    lastScrollTop = scrollTop;
+})
+
 //TYPED //
 var typed= new Typed('.typed',{
 strings: ["Bonjour,je m'apelle Faustine",
