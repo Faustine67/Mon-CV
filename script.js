7638a8480b251f2e35b90f751d780907ccda28d8
Faustine67 <michelfaustine@gmail.com>
2023-03-05 16:57:27 +0100

    add js

---
+++ script.js
@@ -0,0 +1,7 @@
+//TYPED
+var typed= new Typed('.typed',{
+strings: ["Bonjour,je m'apelle Faustine",
+"Je suis passionnée par le digital, le web et les nouvelles technologies. J'ai eu la chance de pouvoir évoluer dans le marketing digital avant de me tourner à présent vers le développement Web et Web Mobile. Je cherche une entreprise dans laquelle je pourrais m'épanouir pour un stage de deux mois."],
+typeSpeed: 20,
+
+});
